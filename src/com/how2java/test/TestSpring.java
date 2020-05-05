379329e3d5844f99dc3a98fef6c5e69826abe549
yinqiyq <yinqiyq@163.com>
2020-05-05 22:33:26 +0800

    spring aop

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -1,6 +1,7 @@
 package com.how2java.test;
 
 import com.how2java.pojo.Product;
+import com.how2java.service.ProductService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,9 +16,12 @@ public class TestSpring {
 //
 //		System.out.println(c.getName());
 
-		Product p = (Product) context.getBean("p");
+//		Product p = (Product) context.getBean("p");
+//
+//		System.out.println(p.getName());
+//		System.out.println(p.getCategory().getName());
 
-		System.out.println(p.getName());
-		System.out.println(p.getCategory().getName());
+		ProductService s = (ProductService) context.getBean("s");
+		s.doSomeService();
 	}
 }
