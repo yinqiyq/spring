2bd9165f65a6d2034f5d615429e43dae7d21cde4
yinqiyq <yinqiyq@163.com>
2020-05-06 22:46:02 +0800

    spring 测试注解

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -2,15 +2,31 @@ package com.how2java.test;
 
 import com.how2java.pojo.Product;
 import com.how2java.service.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.how2java.pojo.Category;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
 public class TestSpring {
 
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
+	@Autowired
+	Category c;
+
+	@Test
+	public void test(){
+		System.out.println(c.getName());
+	}
+
+//	public static void main(String[] args) {
+//		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationContext.xml" });
 
 //		Category c = (Category) context.getBean("c");
 //
@@ -21,7 +37,10 @@ public class TestSpring {
 //		System.out.println(p.getName());
 //		System.out.println(p.getCategory().getName());
 
-		ProductService s = (ProductService) context.getBean("s");
-		s.doSomeService();
-	}
+//		ProductService s = (ProductService) context.getBean("s");
+//		s.doSomeService();
+
+
+
+//	}
 }
